ac42cba777ff2acb0e22b2729ad7457c684a414e
Thomas Wilburn <news@thomaswilburn.net>
2016-10-18 14:28:36 -0700

    Scroll effects work

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,42 @@
 // require("./lib/ads");
 // var track = require("./lib/tracking");
 
-require("component-responsive-frame/child");
 require("./calculator");
+
+var $ = require("./lib/qsa");
+var debounce = require("./lib/debounce");
+var Camera = require("savage-camera");
+var savage = require("savage-query");
+
+var keyStage = document.querySelector("main.scroll-content");
+var map = document.querySelector(".backdrop svg");
+var camera = new Camera(map);
+var stages = $(".layer").reverse();
+var current = null;
+var existing = document.querySelector("#Existing");
+
+var onScroll = function() {
+  var scrollBounds = keyStage.getBoundingClientRect();
+  for (var i = 0; i < stages.length; i++) {
+    var stage = stages[i];
+    var bounds = stage.getBoundingClientRect();
+    if (bounds.top < window.innerHeight && bounds.bottom > 0) {
+      var layerID = stage.getAttribute("data-layer");
+      if (layerID == current) return;
+      var layer = document.querySelector("#" + layerID);
+      if (!layer) return;
+      savage(map).addClass("zoomed");
+      var active = document.querySelector(".activated");
+      if (active) savage(active).removeClass("activated");
+      savage(layer).addClass("activated");
+      current = layerID;
+      camera.zoomTo(layer, 200, 500);
+      return;
+    }
+  }
+  savage(map).removeClass("zoomed");
+  camera.zoomTo(existing, 50, 500);
+}
+
+window.addEventListener("scroll", debounce(onScroll));
+onScroll();
